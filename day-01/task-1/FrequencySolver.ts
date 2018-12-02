a933103a5b1c58d703ec1efe7228844632d75e28
Alasdair Collinson <blalasaadri@googlemail.com>
2018-12-02 14:58:47 +0100

    Solved day 1, task 1

--- day-01/task-1/FrequencySolver.ts
+++ day-01/task-1/FrequencySolver.ts
@@ -1,8 +1,22 @@
+import { readFileSync } from 'fs';
+
 class FrequencySolver {
 
+    private readInput(): number[] {
+        return readFileSync('./day-01/task-1/input.txt', 'utf-8')
+            .split('\n')
+            .filter(Boolean)
+            .map(num => parseInt(num));
+    }
+
     public solve(): number {
-        return 0;
+        let input: number[] = this.readInput();
+        //console.log(input);
+        return input.reduce(
+            (previousValue, currentValue) => previousValue + currentValue
+        );
     }
 }
 
-console.log((new FrequencySolver()).solve());
+let solution = (new FrequencySolver()).solve();
+console.log(solution);
